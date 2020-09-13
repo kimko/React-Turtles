714e37343f30836ddddbc0441f9734c45ccac6f9
Kim Kopowski <kim.kopowski@gmail.com>
2020-09-13 11:40:33 -0700

    Updates

---
+++ precache-manifest.5ce534ddec8f4427c5e5e41b484c4761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abf8d9f84b05ea2b6f781eff143cf98",
+    "url": "/React-Turtles/index.html"
+  },
+  {
+    "revision": "642fb751d41160c77be3",
+    "url": "/React-Turtles/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8273666ca8f5452cced7",
+    "url": "/React-Turtles/static/js/2.443d61f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Turtles/static/js/2.443d61f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642fb751d41160c77be3",
+    "url": "/React-Turtles/static/js/main.4ea1d690.chunk.js"
+  },
+  {
+    "revision": "706f0d4c1b0d873adcc2",
+    "url": "/React-Turtles/static/js/runtime-main.5ac95c40.js"
+  }
+]);
