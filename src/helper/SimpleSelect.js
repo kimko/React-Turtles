060a86c2e209ecad5f932fd8d9ae5cb5d20a7ffd
Kim Kopowski <kim.kopowski@gmail.com>
2020-09-21 17:28:01 -0700

    Add x and y selector

---
+++ src/helper/SimpleSelect.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const SimpleSelect = (props) => {
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    props.setValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">{props.title}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={props.value}
+          onChange={handleChange}
+        >
+          <MenuItem value={"Annuli"}>Annuli</MenuItem>
+          <MenuItem value={"Weight"}>Weight</MenuItem>
+          <MenuItem value={"Carapace"}>Carapace</MenuItem>
+          <MenuItem value={"Plastron"}>Plastron</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
+
+export { SimpleSelect };
--- src/vis/Explore.js
+++ src/vis/Explore.js
@@ -3,13 +3,10 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import {
-  VictoryBar,
   VictoryChart,
   VictoryScatter,
-  VictoryAxis,
   VictoryLine,
   VictoryTheme,
-  VictoryGroup,
   VictoryLegend,
 } from "victory";
 
@@ -17,29 +14,36 @@ import Alert from "@material-ui/lab/Alert";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from '@material-ui/core/Grid';
 
 import useStyles from "../helper/styles";
 import Title from "../helper/Title";
+import { SimpleSelect } from "../helper/SimpleSelect";
 
 const ExploreScatter = (props) => {
   const [alert, setAlert] = useState("");
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState();
-  const [xdim, setXDim] = useState('Annuli');
-  const [ydim, setYDim] = useState('Weight');
+  const [xDim, setXDim] = useState('Annuli');
+  const [yDim, setYDim] = useState('Weight');
   useEffect(() => {
     (async () => {
       try {
+        // TODO: toggle this via configuration 
         // const res = await axios.get(
-        //   `https://bmd-micro.herokuapp.com/getTwoDimensionsPerGenderVictory`
+        //   `http://0.0.0.0:5000/getTwoDimensionsPerGenderVictory?dim1=${xDim}&dim2=${yDim}`,
+        //   {
+        //     headers: {
+        //       'Access-Control-Allow-Origin': '*',
+        //     },}
         // );
         const res = await axios.get(
-          `http://0.0.0.0:5000/getTwoDimensionsPerGenderVictory?dim1=${xdim}&dim2=${ydim}`
+          `https://bmd-micro.herokuapp.com/getTwoDimensionsPerGenderVictory?dim1=${xDim}&dim2=${yDim}`
         );
         const turtleData = res.data.data.turtles;
         console.log(turtleData);
         console.log("done");
-        console.log(turtleData["f"][0]);
+        console.log(turtleData["f"].data[0]);
         setData(turtleData);
         setLoading(false);
       } catch (err) {
@@ -47,11 +51,13 @@ const ExploreScatter = (props) => {
         setAlert(err.message);
       }
     })();
-  }, []);
+  }, [xDim, yDim]);
 
   const classes = useStyles();
 
-  const jitter = () => {
+  const jitter = (dim) => {
+    if (dim !== 'Annuli')
+      return 0;
     if (Math.floor(Math.random() * 2))
       return Math.floor(Math.random() * 50) /100;
     else
@@ -65,6 +71,10 @@ const ExploreScatter = (props) => {
       <Container className={classes.container}>
         <Paper>
           <Title>Explore</Title>
+          <Grid container justify="left" spacing={0}>
+            <SimpleSelect title="X Axis" setValue={setXDim} value={xDim} />
+            <SimpleSelect title="Y Axis" setValue={setYDim} value={yDim} />
+          </Grid>
           {loading && <CircularProgress />}
 
           {/* wrapper component that plots all of its children on the same scale.  */}
@@ -72,37 +82,51 @@ const ExploreScatter = (props) => {
             <VictoryChart
               theme={VictoryTheme.material}
               //   domain={{ x: [0, 5], y: [0, 7] }}
-              width={800}
+              width={1000}
+              height={500}
             >
+              <VictoryLegend
+                x={125}
+                y={10}
+                orientation="horizontal"
+                gutter={20}
+                style={{ border: { stroke: "black" } }}
+                colorScale={["#ff5252", "#1565c0", "#43a047"]}
+                data={[{ name: "female" }, { name: "male" }, { name: "???" }]}
+              />
               <VictoryScatter
-                style={{ data: { fill: "red" } }}
+                style={{ data: { fill: "#ff5252" } }}
                 size={3}
                 data={data["f"].data}
-                y={'Weight'}
-                x={(d) => d.Annuli + jitter()}
+                x={(d) => d[xDim] + jitter(xDim)}
+                y={(d) => d[yDim] + jitter(yDim)}
               />
-              {/* <VictoryLine
+              <VictoryLine
                 data={data["f"].trend.data}
-                style={{ data: { stroke: "red" } }}
+                style={{ data: { stroke: "#ff5252" } }}
               />
               <VictoryScatter
-                style={{ data: { fill: "blue" } }}
+                style={{ data: { fill: "#1565c0" } }}
                 size={3}
                 data={data["m"].data}
+                x={(d) => d[xDim] + jitter(xDim)}
+                y={(d) => d[yDim] + jitter(yDim)}
               />
               <VictoryLine
                 data={data["m"].trend.data}
-                style={{ data: { stroke: "blue" } }}
+                style={{ data: { stroke: "#1565c0" } }}
               />
               <VictoryScatter
-                style={{ data: { fill: "green" } }}
+                style={{ data: { fill: "#43a047" } }}
                 size={3}
                 data={data["unknown"].data}
+                x={(d) => d[xDim] + jitter(xDim)}
+                y={(d) => d[yDim] + jitter(yDim)}
               />
               <VictoryLine
                 data={data["unknown"].trend.data}
-                style={{ data: { stroke: "green" } }}
-              /> */}
+                style={{ data: { stroke: "#43a047" } }}
+              />
             </VictoryChart>
           )}
         </Paper>
